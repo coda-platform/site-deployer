b69e3603706c925de2351101419cbdad3c82f848
Louis Mullie <louis.mullie@gmail.com>
2023-10-24 22:30:30 -0400

    Deployment guide

--- deploy.js
+++ deploy.js
@@ -40,15 +40,24 @@ const services = [
 execSync(`caprover logout -n ${process.env.CAPROVER_NAME} || true`)
 
 for (const service of services) {
-    execSync(
-        `rm -rf ${service.name} 2> /dev/null || true && ` +
-        `git clone ${service.url} && ` +
-        `cd ${service.name} && ` +
-        `cp ../envs/${service.name}.env .env && ` +
-        `echo "\n" >> .env && ` +
-        `cat ../.env >> .env && ` +
-        `npm install && ` +
-        `npm run caprover:setup && ` +
-        `npm run caprover:deploy && ` +
-        `cd .. && rm -rf ${service.name}`, { stdio: 'inherit' })
+    try {
+        execSync(
+            `rm -rf ${service.name} 2> /dev/null || true && ` +
+            `git clone ${service.url} && ` +
+            `cd ${service.name} && ` +
+            `cp ../envs/${service.name}.env .env && ` +
+            `echo "\n" >> .env && ` +
+            `cat ../.env >> .env && ` +
+            `npm install && ` +
+            `npm run caprover:setup && ` +
+            `npm run caprover:deploy && ` +
+            `cd .. && rm -rf ${service.name}`, { stdio: 'inherit' })
+    } catch (err) {
+        if (err.message.includes('App Name already exists.')) {
+            console.log(`App ${service.name} already exists. Skipping...`)
+        } else {
+            console.error(`Error deploying ${service.name}: ${err.message}`)
+        }
+        execSync(`rm -rf ${service.name}`, { stdio: 'inherit' })
+    }
 }
